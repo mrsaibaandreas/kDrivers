a041ac3d81f2cf3a4bd516246bf640da899cf392
mrsaibaandreas <andreasqp@gmail.com>
2023-03-16 23:45:07 +0200

    First real initial commit

--- pwm_driver.c
+++ pwm_driver.c
@@ -5,20 +5,44 @@
 #include <linux/fs.h>
 #include <linux/cdev.h>
 #include <linux/device.h>
-#include<linux/slab.h>                 //kmalloc()
-#include<linux/uaccess.h>              //copy_to/from_user()
-#include<linux/sysfs.h> 
-#include<linux/kobject.h> 
- #include <linux/err.h>
+#include <linux/slab.h>                 //kmalloc()
+#include <linux/uaccess.h>              //copy_to/from_user()
+#include <linux/sysfs.h> 
+#include <linux/kobject.h> 
+#include  <linux/err.h>
 
+/* The code was inspired from https://gist.github.com/BtbN/9665914 
+ * https://embetronicx.com/tutorials/linux/device-drivers/sysfs-in-linux-kernel/
+ */
 
-dev_t dev = 0;
-static struct class *dev_class;
-static struct cdev etx_cdev;
-struct kobject *kobj_ref;
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Andreas Mrsaiba");
+MODULE_DESCRIPTION("Userspace PWM management device");
+
+//dev_t dev = 0;
+//static struct class *dev_class;
+//static struct cdev etx_cdev;
+//struct kobject *kobj_ref;
 /*
 ** Function Prototypes
 */
-static int      __init etx_driver_init(void);
-static void     __exit etx_driver_exit(void);
+static int      __init pwm_driver_init(void);
+static void     __exit pwm_driver_exit(void);
+/* TO DO
+ * -> create a /sys/kernel/ entry for the driver
+   -> write it's fops functions
+ */
 
+
+static int	__init pwm_driver_init(void) {
+	printk("Init\n");
+	return 0;
+}
+
+static void 	__exit pwm_driver_exit(void) {
+	printk("Exiting\n");
+	return;
+}
+
+module_init(pwm_driver_init);
+module_exit(pwm_driver_exit);
